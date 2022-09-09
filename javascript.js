082a318fb6c1c41516912f5a295dbbd42267b8f0
Morgan Winkler <winkler.morgan1553@gmail.com>
2022-09-09 17:03:40 -0400

    Add single round

--- javascript.js
+++ javascript.js
@@ -2,8 +2,39 @@ const options = ['rock' , 'paper' , 'scissors'];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
+    console.log(choice);
     return choice;
+    
+    
 }
 
-var playerSelection = prompt("Please type rock, paper, or scissors");
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return 'Tie';
+    }
+    else if(
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+    ){
+        return 'Player';
+    }
+    else {
+        return 'Computer';
+    }
+}
+function playRound(playerSelection,computerSelection){
+    const result = checkWinner(playerSelection,computerSelection);
+    if(result == 'Tie'){
+        return 'We tie!';
+    }
+    else if(result == 'Player'){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
 
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
